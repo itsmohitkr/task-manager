17c134c429d7fd10b4d4fd53f506ba54086e9f39
Mohit kumar <50844220+itsmohitkr@users.noreply.github.com>
2021-07-25 22:57:01 +0530

    delete btn function on card ,working fine

--- public/js/script.js
+++ public/js/script.js
@@ -23,6 +23,10 @@ function cancel() {
 
 function addtask() {
     console.log(text_data.value);
+    if (text_data.value == "") {
+        alert("please enter task field.")
+        return;
+    }
     // append to dom
     appendtodom(text_data.value);
     
@@ -38,15 +42,21 @@ function addtask() {
 }
 
 function appendtodom(data) {
+    console.log(data)
     let divelement = document.createElement('div');
     divelement.classList.add("task_added");
     
     let x = `onclick = "progress(this)"`
-    let y = `onclick = "done(this)"`
+    let y = `onclick = "directTodone(this)"`
+    let deletebtn = `onclick = "deletecard(this)"`
     let markup = `
                     <div class="icon">
-                    <i class="far fa-caret-square-down"></i>
-                    <i class="fas fa-trash-alt"></i>
+                        <div>
+                            <i class="far fa-caret-square-down"></i>
+                        </div>
+                        <div ${deletebtn}>
+                            <i class="fas fa-trash-alt"></i>
+                        </div>
                     </div>
                     <p class="task_text" style="overflow-wrap: break-word;">${data}</p>
                     <div class="line"></div>
@@ -58,6 +68,26 @@ function appendtodom(data) {
     divelement.innerHTML = markup
     new_task.prepend(divelement)
     cancel();
+}
+
+function directTodone(evnt) {
+    let task_text = evnt.parentNode.parentNode.children[1].innerText;
+    console.log(task_text)
+    evnt.parentNode.parentNode.remove();
+    deletenewtaskfromDb(task_text);
+    appendtoCompleteddom(task_text);
+    let json_task_text = {
+        task_text:task_text
+    }
+    synctocompleteddiv(json_task_text);
+}
+
+function deletecard(evnt) {
+    alert("oops🥴! you are deleting before completing this task.")
+    let task_text = evnt.parentNode.parentNode.children[1].innerText;
+    console.log(task_text)
+    evnt.parentNode.parentNode.remove();
+    deletenewtaskfromDb(task_text);
 }
 
 // function progress(evnt) {
@@ -72,13 +102,17 @@ function appendtodom(data) {
 function appendtoinprogressdom(data) {
     let divelement = document.createElement('div');
     divelement.classList.add("inprogress");
-
     // let x = `onclick = "progress(this)"`
     let y = `onclick = "done(this)"`
+    let delete_card_progress = `onclick = "deletecardinprogress(this)"`
     let markup = `
                     <div class="icon">
-                    <i class="far fa-caret-square-down"></i>
-                    <i class="fas fa-trash-alt"></i>
+                        <div>
+                            <i class="far fa-caret-square-down"></i>
+                        </div>
+                        <div ${delete_card_progress}>
+                            <i class="fas fa-trash-alt"></i>
+                        </div>
                     </div>
                     <p class="task_text" style="overflow-wrap: break-word;">${data}</p>
                     <div class="line"></div>
@@ -97,14 +131,19 @@ function progress(evnt) {
     let task_text = evnt.parentNode.parentNode.children[1].innerText;
     evnt.parentNode.parentNode.remove();
     deletenewtaskfromDb(task_text);
-     let divelement = document.createElement('div');
+    let divelement = document.createElement('div');
     divelement.classList.add("inprogress");
     let z = `onclick = "done(this)"`
+    let delete_card_progress = `onclick = "deletecardinprogress(this)"`
 
         let progressmarkup = `
                     <div class="icon">
-                    <i class="far fa-caret-square-down"></i>
-                    <i class="fas fa-trash-alt"></i>
+                        <div>
+                            <i class="far fa-caret-square-down"></i>
+                        </div>
+                        <div ${delete_card_progress}>
+                            <i class="fas fa-trash-alt"></i>
+                        </div>
                     </div>
                     <p class="task_text" style="overflow-wrap: break-word;">${task_text}</p>
                     <div class="line"></div>
@@ -119,6 +158,16 @@ function progress(evnt) {
         task_text: task_text
     }
     synctoprogressdiv(text_in_progress);
+}
+
+function deletecardinprogress(evnt) {
+let x = prompt(`Do you really want to delete before completing this task 🤕?
+( y for 'yes')`)
+    if(x==="y"){
+        let task_text = evnt.parentNode.parentNode.children[1].innerText;
+        evnt.parentNode.parentNode.remove();
+        deleteNewTaskfromProgressDb(task_text);
+    }
 }
 
 function synctoprogressdiv(text_in_progress) {
@@ -137,9 +186,11 @@ function synctoprogressdiv(text_in_progress) {
 }
 
 function deletenewtaskfromDb(delete_task) {
+    console.log(delete_task)
     let tobedeleted = {
         delete_task: delete_task
     }
+    console.log(tobedeleted)
     const headers = {
         'Content-Type': 'application/json'
     }
@@ -162,10 +213,15 @@ function done(evnt) {
 
     let divelement = document.createElement('div');
     divelement.classList.add("completed");
+    let delete_card_done = `onclick = "deletecardinCompleted(this)"`
     let completedmarkup = `
                     <div class="icon">
-                    <i class="far fa-caret-square-down"></i>
-                    <i class="fas fa-trash-alt"></i>
+                        <div>
+                            <i class="far fa-caret-square-down"></i>
+                        </div>
+                        <div ${delete_card_done}>
+                            <i class="fas fa-trash-alt"></i>
+                        </div>
                     </div>
                     <p class="task_text" style="overflow-wrap: break-word;">${task_text}</p>
                     <div class="line"></div>
@@ -184,13 +240,26 @@ function done(evnt) {
 
 }
 
+function deletecardinCompleted(evnt) {
+    let task_text = evnt.parentNode.parentNode.children[1].innerText;
+    evnt.parentNode.parentNode.remove();
+    deleteNewTaskfromCompletedDb(task_text);
+}
+
+
 function appendtoCompleteddom(data) {
     let divelement = document.createElement('div');
     divelement.classList.add("completed");
+    let delete_card_done = `onclick = "deletecardinCompleted(this)"`
+
     let completedmarkup = `
                     <div class="icon">
-                    <i class="far fa-caret-square-down"></i>
-                    <i class="fas fa-trash-alt"></i>
+                        <div>
+                            <i class="far fa-caret-square-down"></i>
+                        </div>
+                        <div ${delete_card_done}>
+                            <i class="fas fa-trash-alt"></i>
+                        </div>
                     </div>
                     <p class="task_text" style="overflow-wrap: break-word;">${data}</p>
                     <div class="line"></div>
@@ -218,6 +287,24 @@ function deleteNewTaskfromProgressDb(delete_task_from_progress) {
         .then(response => response.json())
         .then(result => {
             console.log("deleted form progress ")
+        })
+}
+
+function deleteNewTaskfromCompletedDb(delete_task_from_completed) {
+    let tobedeletedInCompleted = {
+        delete_task_from_completed: delete_task_from_completed
+    }
+    const headers = {
+        'Content-Type': 'application/json'
+    }
+    fetch('/api/completed', {
+            method: 'Delete',
+            body: JSON.stringify(tobedeletedInCompleted),
+            headers
+        })
+        .then(response => response.json())
+        .then(result => {
+            console.log("deleted form completed ")
         })
 }
 
@@ -258,7 +345,7 @@ function fetchComments() {
         .then(res => res.json())
         .then(result => {
             result.forEach((comingdata) => {
-                console.log(comingdata);
+                console.log(comingdata.newtask);
                 appendtodom(comingdata.newtask)
             })
         })
--- src/server.js
+++ src/server.js
@@ -12,7 +12,7 @@ const dbConnect = require('./db')
 dbConnect()
 
 // require cooment module
-const Task = require('../models/task')
+const Task_list = require('../models/task')
 const Progress_list = require('../models/progress')
 const Completed_list = require('../models/completed')
 
@@ -39,7 +39,7 @@ app.get('/:index', (req, res) => {
 
 // Routes 
 app.post('/api/tasks', (req, res) => {
-    const addTask = new Task({
+    const addTask = new Task_list({
         newtask: req.body.task
         // username: req.body.username
     })
@@ -47,25 +47,24 @@ app.post('/api/tasks', (req, res) => {
         res.send(response)
     })
 })
-app.delete('/api/tasks', (req, res) => {
+app.get('/api/tasks', (req, res) => {
+    Task_list.find().then(function (task) {
+        res.send(task)
+    })
+})
+app.delete('/api/tasks', (req, res) => {    
     const query = {
         "newtask": req.body.delete_task
     };
     console.log(query);
-    Task.findOneAndDelete(query)
-    .then(result => console.log(`Deleted.`))
-    .catch(err => console.error(`Delete failed with error: ${err}`))
-
+    Task_list.findOneAndDelete(query)
+        .then(result => console.log(`Deleted.`))
+        .catch(err => console.error(`Delete failed with error: ${err}`))
 })
 
-app.get('/api/tasks', (req, res) => {
-    Task.find().then(function (task) {
-        res.send(task)
-    })
-})
 
 app.post('/api/progress', (req, res) => {
-       const add_to_Progress_list = new Progress_list({
+    const add_to_Progress_list = new Progress_list({
            new_progress_task: req.body.task_text
        })
     add_to_Progress_list.save().then(response => {
@@ -108,7 +107,7 @@ app.get('/api/completed', (req, res) => {
 
 app.delete('/api/completed', (req, res) => {
     const query = {
-        "new_progress_task": req.body.delete_task_from_progress
+        "new_completed_task": req.body.delete_task_from_completed
     };
     console.log(query);
     Completed_list.findOneAndDelete(query)
